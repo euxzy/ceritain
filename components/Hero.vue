25b223ca660cc7ee1380d0ed321c889316a791d5
euxzy <mhd.rijal203@gmail.com>
2023-04-04 21:51:26 +0700

    [ + ] update config and create hero section

---
+++ components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container">
+    <div class="pt-24">
+      <div class="w-72 mx-auto mb-2">
+        <img class="w-full" src="~/assets/images/cats.png" alt="Ceritain">
+      </div>
+      <div class="w-max mx-auto text-center font-medium text-lg leading-tight">
+        <p>Ceritain</p>
+        <p>Ceritakan semua yang pengen kamu ceritakan!</p>
+      </div>
+      <div class="flex justify-center gap-[14px] mt-14">
+        <NuxtLink to="/login" class="bg-accent-2 py-0.5 px-6 rounded-lgm border border-black drop-shadow-br">Login</NuxtLink>
+        <NuxtLink to="/register" class="bg-accent-9 py-0.5 px-[22px] rounded-lgm border border-black drop-shadow-br">Daftar</NuxtLink>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -11,33 +11,7 @@
 </script>
 
 <template>
-  <section class="container">
-    <div class="w-max mx-auto pt-16 pb-8 font-medium text-lg">
-      <h1 class="relative font-extrabold text-5xl px-3 w-max mx-auto mb-0.5">
-        Cerita 
-        <span class="absolute w-5 h-5 pl-0.5 bg-black text-white rounded-full text-xs font-medium flex items-center top-0 -right-1">.in</span>
-      </h1>
-      <p>Ceritakan semua yang pengen kamu ceritakan!</p>
-    </div>
-
-    <PostForm />
-
-    <div class="mt-12">
-      <BtnNav />
-    </div>
-
-    <div class="mt-12">
-      <div class="w-[1024px] mx-auto">
-        <div class="flex justify-center flex-wrap gap-y-6 gap-x-5" v-if="posts.length > 0">
-          <Card 
-            v-for="(post, idx) in posts"
-            :key="idx"
-            :post="post"
-          />
-        </div>
-      </div>
-    </div>
-
-    <Footer />
+  <section>
+    <Hero />
   </section>
 </template>
