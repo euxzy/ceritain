a9a98e580bdf8c1bdb46530ddcea56899d6ed490
euxzy <mhd.rijal203@gmail.com>
2023-04-11 22:15:30 +0700

    [ + ] comments

---
+++ components/FormComment.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="border border-black rounded-lgm drop-shadow-br bg-white">
+    <form class="p-4">
+      <textarea 
+        name="comment" 
+        id="comment" 
+        placeholder="Tulis Komentarmu..."
+        class="outline-none border border-black w-full rounded-lg p-2 min-h-[2rem] mb-1.5"
+      ></textarea>
+      <button class="w-full text-center bg-accent-9 rounded-lg border border-black py-2 font-bold">Beri Komentar!</button>
+    </form>
+  </section>
+</template>
