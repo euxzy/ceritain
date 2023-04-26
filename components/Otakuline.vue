bb3d2406ed600534e4625ee63160ca9999c287ce
euxzy <mhd.rijal203@gmail.com>
2023-04-26 17:47:33 +0700

    [ ~ ] responsive

--- components/Otakuline.vue
+++ components/Otakuline.vue
@@ -21,7 +21,7 @@
       >
         <div class="absolute w-3 h-3 bg-white border border-black inset-y-0 my-auto left-0 rounded-full translate-x-4 lg:translate-x-7"></div>
         <p
-          class="px-10 md:px-0"
+          class="px-10 md:px-0 truncate"
           v-html="item?.title?.rendered"
         >
         </p>
