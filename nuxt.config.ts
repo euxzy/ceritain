4aa3029baa9106da781ee847d7f8acb179a39253
euxzy <mhd.rijal203@gmail.com>
2023-05-02 23:25:21 +0700

    [ + ] favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Ceritain | Otakuline.id'
+      title: 'Ceritain | Otakuline.id',
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', sizes: '16x16x', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ]
     }
   },
   routeRules: {
