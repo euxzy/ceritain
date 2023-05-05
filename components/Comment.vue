2ec06094f530eb259ba8ccb763ccbb6178d8170c
euxzy <mhd.rijal203@gmail.com>
2023-05-05 10:26:46 +0700

    [ = ] fix comment

--- components/Comment.vue
+++ components/Comment.vue
@@ -5,6 +5,6 @@
 <template>
   <section class="mb-5 rounded-lg px-4 py-2 bg-secondary drop-shadow-br">
     <p class="text-xs mb-1">Dari <NuxtLink :to="`/user/${item?.user?.username}`" class="text-base font-bold">{{ item?.user?.name }}</NuxtLink></p>
-    <p>{{ item?.content }}</p>
+    <p v-html="item?.content"></p>
   </section>
 </template>
